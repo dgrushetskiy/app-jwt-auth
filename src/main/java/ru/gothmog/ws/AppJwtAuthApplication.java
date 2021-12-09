d0b200daf600d4013ad9ef3f59944a4facaadbd3
dgrushetskiy <dgrushetskiy@gmail.com>
2021-12-09 12:48:08 +0300

    add project spring

---
+++ src/main/java/ru/gothmog/ws/AppJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppJwtAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gothmog/ws/AppJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
