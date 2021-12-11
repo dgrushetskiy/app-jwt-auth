9561f60d6d52f05525df8a3c0394c6d2bcc5ada4
dgrushetskiy <dgrushetskiy@gmail.com>
2021-12-12 01:08:36 +0300

    add admin realization

---
+++ src/main/java/ru/gothmog/ws/rest/controller/AdminRestControllerV1.java
@@ -0,0 +1,40 @@
+package ru.gothmog.ws.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.gothmog.ws.core.model.auth.User;
+import ru.gothmog.ws.core.service.UserService;
+import ru.gothmog.ws.rest.dto.AdminUserDto;
+
+/**
+ * REST controller for ROLE_ADMIN requests.
+ */
+@RestController
+@RequestMapping(value = "/api/v1/admin/")
+public class AdminRestControllerV1 {
+
+    private final UserService userService;
+
+    @Autowired
+    public AdminRestControllerV1(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "users/{id}")
+    public ResponseEntity<AdminUserDto> getUserById(@PathVariable(name = "id") Long id) {
+        User user = userService.getById(id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        AdminUserDto result = AdminUserDto.fromUser(user);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
